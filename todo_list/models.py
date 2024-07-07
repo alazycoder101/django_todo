679738dea1b965be7a911c7b7f43abc35227784e
alazycoder <alazycoder101@gmail.com>
2024-07-08 00:07:16 +0100

    fix: error of todo_list_note table

--- todo_list/models.py
+++ todo_list/models.py
@@ -5,5 +5,7 @@ class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=500)
 
+    class Meta:
+        db_table = 'notes'
     def __str__(self):
         return self.title
