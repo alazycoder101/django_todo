4340abab8913519b1984ea537f2d6228e69a7a36
alazycoder <alazycoder101@gmail.com>
2024-07-07 23:40:15 +0100

    add models and so on

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from .models import Note
+from .serializers import NoteSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
