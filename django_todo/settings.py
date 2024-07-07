5af0cd835d928401be274c156c06f38db73aae37
alazycoder <alazycoder101@gmail.com>
2024-07-07 23:56:49 +0100

    fix: Model class django.contrib.auth.models.Permission doesn't declare an explicit app_label and isn't in an application in INSTALLED_APPS.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'rest_framework',
     'todo_list',
